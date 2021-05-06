9da991a6e00370f97372316033ff71fc4d9d5412
Estehkak <istihkak.benhar@gmail.com>
2021-05-07 00:28:53 +0200

    Create function for authentification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,4 +10,36 @@ class UserController extends Controller
 {
     const MODEL = User::class;
     use CrudActions;
+
+    public function authentification(Request $request){
+        try{
+           $email=$request->input('email');
+           $password=$request->input('password');
+
+           $user=User::where('email',$email)->first();
+           if($user){
+            if($user->password === $password){
+                $rslt['status']=true;
+                return response(true,200);
+            }
+            else{
+                $rslt['status']=false;
+                return response(false,200);
+            }
+           }
+           else{
+            $rslt['status']=false;
+            $rslt['message']='user not found';
+            return response(false,200);
+           }
+        }
+        catch(\Illuminate\Databse\QueryException $ex){
+        	$res['status']=false;
+        	$res['message']=$ex->getMessages();
+
+            return response($res,500);
+        }
+        }
 }
+    
+
